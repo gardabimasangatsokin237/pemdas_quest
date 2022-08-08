9aa2b9ad5c6b41efafb37487e299ce4b900e481b
gardabimasangatsokin237 <110508885+gardabimasangatsokin237@users.noreply.github.com>
2022-08-07 18:51:59 -0700

    Add files via upload

---
+++ Python1.py
@@ -0,0 +1,13 @@
+#belajar output
+ 
+
+print("Hello world")
+print("nama saya Garda Bima")
+print("hobby saya bermain game")
+print("rumah saya di kampung sugutamu rt2/28 no67")
+print("makanan kesukaan saya adalah Mie Ayam")
+print("cita cita saya ingin menjadi polisi")
+print("saya bersekolah di SMK Harapan Bangsa")
+print("saya pernah bersekolah di SMP Mutiara Bangsa")
+print("saya lahir di Depok")
+print("saya sekarang duduk dibangku kelas 10 SMK")
---
+++ Python2.py
@@ -0,0 +1,11 @@
+#Belajar output
+
+nama = "Garda" # Garda adalah data , nama adalah variable
+umur = 15 
+alamat = "sukmajaya baktijaya kota Depok"
+
+print(nama)
+print(umur)
+print(alamat)
+
+print("nama saya adalah" + nama + "umur saya adalah" + str("umur") + "alamat saya" + alamat)
