649f63fa72e79fb8c9d77d76a8312b59dc0ef270
gardabimasangatsokin237 <110508885+gardabimasangatsokin237@users.noreply.github.com>
2022-08-11 20:38:54 -0700

    Add files via upload

---
+++ UPRAK Garda Bima Pemdas.py
@@ -0,0 +1,9 @@
+nama_depan = input("masukkan nama depan: ")
+nama_belakang = input("masukkan nama belakang: ")
+
+print("nama anda adalah", nama_depan, nama_belakang)
+
+celcius = int(input("masukkan suhu celcius: "))
+ream = (4/5 * celcius) + 32
+
+print(celcius, "celcius", ream, "reamur")
